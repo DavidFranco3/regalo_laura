e1ff6cf3ad33b259784396be198cc2c6d72f8b96
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-06-16 11:40:57 -0600

    primera version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Heart from './components/heart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>¡Feliz cumpleaños, Laura!</h1>
+        <p>Esta página es un regalo especial para ti.</p>
       </header>
+      <main>
+        <Heart/>
+      </main>
+      <footer>
+        <p>Con amor, David</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/heart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './heart.css';
+
+const Heart = () => {
+    return (
+        <div className="corazon-container">
+            <div className="corazon"></div>
+        </div>
+    );
+};
+
+export default Heart;
